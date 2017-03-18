b5640349cd35553b79ab3cc9d4f5125401bf5299
Dante Rangel <dante@gowi.com.mx>
2017-03-18 13:29:29 -0600

    m

--- SegundaClase/auth/login.php
+++ SegundaClase/auth/login.php
@@ -1,9 +1,31 @@
 <?php 
-	include('../conexion/conexion.php');
+	
+	
+	session_start();
+	$_SESSION['correo'];
 	if(isset($_POST['correo']) && isset($_POST['password'] ) ){
-		
+		#echo "Entraste y si existen tus variables";
+		include('../conexion/conexion.php');
+		$sql="SELECT * FROM Usuario where correo='".$_POST['correo']."'";
+		$resultado=mysqli_query($conexion,$sql);
+			if(mysqli_num_rows($resultado)>0){
+				$registro=mysqli_fetch_array($resultado);
+				#echo $registro['password'].' '.md5($_POST['password']);
+				 
+				if(md5($_POST['password'])==$registro['password']){
+						$_SESSION['id']=$registro['id'];
+						$_SESSION['nombre']=$registro['nombre'];
+						$_SESSION['tipo_usuario']=$registro['tipo_usuario'];
+						header('Location: ../dashboard.php');
+				}else{
+					$_SESSION['correo']=$_POST['correo'];
+				header('Location: ../index.php?error=Error&tipo=password');
+				}
+			}else{
+				$_SESSION['correo']=$_POST['correo'];
+				header('Location: ../index.php?error=Error&tipo=correo');
 
-	}else{
-		header('Location : ../index.php?error=Error');
+			}
+
 	}
 ?> 
--- SegundaClase/index.php
+++ SegundaClase/index.php
@@ -1,3 +1,9 @@
+<?php 
+	include('auth/login.php');
+	if(isset($_SESSION['id'])){
+		header('Location: dashboard.php');
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,16 +14,25 @@
 </head>
 <body>
 	<div class="container">
-		<form action="auth/login.php">
+
+		<form action="auth/login.php" method="POST">
 			<div class="row">
 				<div class="col-md-offset-4 col-sm-offset-4 col-lg-offset-4 col-md-4 col-lg-4 col-sm-4 col-xs-12">
-					<div class="form-group">
+				<?php 
+					
+				?>
+					<div class="alert alert-success">
+				  <strong>Success!</strong> You should <a href="#" class="alert-link">read this message</a>.
+				</div>
+				<?php ?>
+					<div class="form-group <?php echo (isset($_GET['error']) && isset($_GET['tipo'])&& $_GET['tipo']=='correo')?' has-error':'' ?>">
 						<label for="">Correo</label>
 							<input type="text" class="form-control" name="correo">
 					</div>
-					<div class="form-group ">
+					<div class="form-group <?php echo (isset($_GET['error']) && isset($_GET['tipo'])&& $_GET['tipo']=='password')?' has-error':'' ?>">
 						<label for="">Contraseña</label>
-							<input type="password" class="form-control" name="pasword">
+							<input type="password" class="form-control" name="password">
+
 					</div>
 					<input type="submit" value="Enviar" class="btn btn-default">
 			</div>
